760e6ac02e1f05f70a6dc6b299e4448bcdd6a1a4
Sanjay Subramaniam <sanjaysubramaniam99@gmail.com>
2023-01-27 02:10:55 +0530

    Initital commit

---
+++ UWPPlayground/Extensions/PopupExtensions.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Media;
+
+namespace UWPPlayground.Extensions
+{
+    public enum Side { Left, Top, Right, Bottom }
+
+    public static class PreferenceOrders
+    {
+        public static Side[] TopBottomLeft = new Side[] { Side.Top, Side.Bottom, Side.Left };
+        public static Side[] TopBottomLeftRight = new Side[] { Side.Top, Side.Bottom, Side.Left, Side.Right };
+    }
+
+    public static class PopupExtension
+    {
+        /// <summary>
+        /// Use this method to show a popup near another FrameworkElement.
+        /// <para>NOTE: MaxWidth and MaxHeight of the popup must be set beforehand.</para>
+        /// </summary>
+        /// <param name="targetElement">
+        /// Framework element to position popup near.
+        /// </param>
+        /// <param name="preferenceOrder">
+        /// Represents the preferred order of <see cref="Side"/> to position popup. 
+        /// For eg: If preferenceOrder is { Side.Left, Side.Right }, extension will try to position the popup to left of <paramref name="targetElement"/> and right it that fails.
+        /// </param>
+        /// <returns>
+        /// True when popup has been positioned in any of the sides in <paramref name="preferenceOrder"/>. False otherwise.
+        /// </returns>
+        public static bool TryShowNear(this Popup popup, FrameworkElement targetElement, Side[] preferenceOrder, double margin = 10)
+        {
+            try
+            {
+                // TO DO: Handle case where popup can be shown outside WindowBounds.
+
+                // Tranform (0, 0) of popup and target element with respect to root page.
+                var popupTransform = popup.TransformToVisual(Window.Current.Content);
+                Point popupCoords = popupTransform.TransformPoint(new Point(0, 0));
+                var targetElementTransform = targetElement.TransformToVisual(Window.Current.Content);
+                Point targetElementCoords = targetElementTransform.TransformPoint(new Point(0, 0));
+
+                // Calculate horizontal and vertical distance between popup and target element.
+                double distanceX = targetElementCoords.X - popupCoords.X; // distanceX > 0 : popup is to left of target element
+                double distanceY = targetElementCoords.Y - popupCoords.Y; // distanceY > 0 : popup is above target element
+
+                Rect windowBounds = Window.Current.Bounds;
+                double? verticalOffsetForAlignment = null;
+                double? horizontalOffsetForAlignment = null;
+
+                foreach (var side in preferenceOrder)
+                {
+                    switch (side)
+                    {
+                        case Side.Left:
+                            // Calculate offset accounting for the popup's final position and check if the popup will be within window bounds.
+                            var horizontalOffset = distanceX - popup.MaxWidth - margin;
+                            if (popupCoords.X + horizontalOffset >= 0)
+                            {
+                                popup.HorizontalOffset = horizontalOffset;
+                                if (!verticalOffsetForAlignment.HasValue)
+                                {
+                                    verticalOffsetForAlignment = GetOffsetForAlignment(popupCoords.Y,
+                                                                                       targetElementCoords.Y,
+                                                                                       distanceY,
+                                                                                       popup.MaxHeight,
+                                                                                       targetElement.ActualHeight,
+                                                                                       windowBounds.Height);
+                                }
+                                popup.VerticalOffset = verticalOffsetForAlignment.Value;
+                                popup.IsOpen = true;
+                                return true;
+                            }
+                            continue;
+
+                        case Side.Right:
+                            horizontalOffset = distanceX + targetElement.ActualWidth + margin;
+                            if (popupCoords.X + horizontalOffset + popup.MaxWidth <= windowBounds.Width)
+                            {
+                                popup.HorizontalOffset = horizontalOffset;
+                                if (!verticalOffsetForAlignment.HasValue)
+                                {
+                                    verticalOffsetForAlignment = GetOffsetForAlignment(popupCoords.Y,
+                                                                                       targetElementCoords.Y,
+                                                                                       distanceY,
+                                                                                       popup.MaxHeight,
+                                                                                       targetElement.ActualHeight,
+                                                                                       windowBounds.Height);
+                                }
+                                popup.VerticalOffset = verticalOffsetForAlignment.Value;
+                                popup.IsOpen = true;
+                                return true;
+                            }
+                            continue;
+
+                        case Side.Top:
+                            var verticalOffset = distanceY - popup.MaxHeight - margin;
+                            if (popupCoords.Y + verticalOffset >= 0)
+                            {
+                                popup.VerticalOffset = verticalOffset;
+                                if (!horizontalOffsetForAlignment.HasValue)
+                                {
+                                    horizontalOffsetForAlignment = GetOffsetForAlignment(popupCoords.X,
+                                                                                         targetElementCoords.X,
+                                                                                         distanceX,
+                                                                                         popup.MaxWidth,
+                                                                                         targetElement.ActualWidth,
+                                                                                         windowBounds.Width);
+                                }
+                                popup.HorizontalOffset = horizontalOffsetForAlignment.Value;
+                                popup.IsOpen = true;
+                                return true;
+                            }
+                            continue;
+
+                        case Side.Bottom:
+                            verticalOffset = distanceY + targetElement.ActualHeight + margin;
+                            if (popupCoords.Y + verticalOffset + popup.MaxHeight <= windowBounds.Height)
+                            {
+                                popup.VerticalOffset = verticalOffset;
+                                if (!horizontalOffsetForAlignment.HasValue)
+                                {
+                                    horizontalOffsetForAlignment = GetOffsetForAlignment(popupCoords.X,
+                                                                                         targetElementCoords.X,
+                                                                                         distanceX,
+                                                                                         popup.MaxWidth,
+                                                                                         targetElement.ActualWidth,
+                                                                                         windowBounds.Width);
+                                }
+                                popup.HorizontalOffset = horizontalOffsetForAlignment.Value;
+                                popup.IsOpen = true;
+                                return true;
+                            }
+                            continue;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// This method calculates the offset needed to align a popup with a target element.
+        /// </summary>
+        /// <param name="popupCoord"></param>
+        /// <param name="targetElementCoord"></param>
+        /// <param name="distance"></param>
+        /// <param name="popupMaxDimension"></param>
+        /// <param name="targetElementDimension"></param>
+        /// <param name="windowDimension"></param>
+        /// <returns></returns>
+        private static double GetOffsetForAlignment(double popupCoord,
+                                                    double targetElementCoord,
+                                                    double distance,
+                                                    double popupMaxDimension,
+                                                    double targetElementDimension,
+                                                    double windowDimension)
+        {
+            double offsetForAlignment;
+            // Calculates the offset needed to align the center of the popup with the center of the target element.
+            double offsetForCenterAlignment = distance - (popupMaxDimension - targetElementDimension) / 2;
+
+            // If the calculated offset positions the popup within the window boundaries, align the center of the popup with the center of the target element.
+            if (popupCoord + offsetForCenterAlignment >= 0 &&
+                popupCoord + offsetForCenterAlignment + popupMaxDimension <= windowDimension)
+            {
+                offsetForAlignment = offsetForCenterAlignment;
+            }
+            else
+            {
+                // If the target element is closer to the left/top side of the window, align the left/top edge of the popup with the left/top edge of the target element.
+                if (targetElementCoord + targetElementDimension / 2 < windowDimension / 2)
+                {
+                    offsetForAlignment = distance;
+                }
+                // If the target element is closer to the right/bottom side of the window, align the right/bottom edge of the popup with the right/bottom edge of the target element.
+                else
+                {
+                    offsetForAlignment = distance - popupMaxDimension + targetElementDimension;
+                }
+            }
+            return offsetForAlignment;
+        }
+    }
+}
---
+++ UWPPlayground/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UWPPlayground.Extensions;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPPlayground
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void OpenPopupBtn_Click(object sender, RoutedEventArgs e)
+        {
+            bool isShown = TestPopup.TryShowNear(targetElement: OpenPopupBtn, preferenceOrder: new Side[] { Side.Bottom, Side.Right }, margin: 8);
+            if (!isShown)
+            {
+                // Extension couldn't find a suitable position matching given preferences. You can handle this case and set a default behaviour.
+            }
+        }
+
+        private void TestTb_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void TestPopup_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            
+        }
+    }
+
+}
